600c94c2f93b607864f8f8b13075bc9f569f3716
Aji Manarul Aziz <ajimanarul@gmail.com>
2021-04-06 23:58:15 +0700

    add exercise

---
+++ I0320119_Exercise6.11.py
@@ -0,0 +1,4 @@
+for i in range(11):
+    print(1, end='')
+    if 1 == 7:
+        break
---
+++ I0320119_Exercise6.2.py
@@ -0,0 +1,10 @@
+# input untuk nilai n
+n = int(input("Masukkan banyak pengulangan : "))
+
+# melakukan pengulangan
+i = 1
+
+while i <= n:
+    print(i)
+    i = i + 1
+ 
